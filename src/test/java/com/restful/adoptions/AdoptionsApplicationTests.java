26250c878f4c27aeb8789339c4945f7dd0187eaa
Damian Morales <damian.morales@janis.im>
2024-11-11 15:35:56 -0300

    first commit

---
+++ src/test/java/com/restful/adoptions/AdoptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.adoptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
