ebcfdf84b3b7a52cbb8268f0ac5645497fb62555
Alejandro Alonso <49244110+aleAlonso90@users.noreply.github.com>
2025-01-14 03:09:51 -0300

    Update RoleRepository.java

--- src/main/java/com/restful/adoptions/user/repository/RoleRepository.java
+++ src/main/java/com/restful/adoptions/user/repository/RoleRepository.java
@@ -2,7 +2,6 @@ package com.restful.adoptions.user.repository;
 
 
 import com.restful.adoptions.user.model.RoleEntity;
-//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
