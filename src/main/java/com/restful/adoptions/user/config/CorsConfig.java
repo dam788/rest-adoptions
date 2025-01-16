873992db3b5d17e29ba1cebdb6a746beb1d5baff
Alejandro Alonso <49244110+aleAlonso90@users.noreply.github.com>
2025-01-16 14:57:59 -0300

    Update CorsConfig.java

--- src/main/java/com/restful/adoptions/user/config/CorsConfig.java
+++ src/main/java/com/restful/adoptions/user/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://tudominio.com")
+                        .allowedOrigins("http://localhost:8080", "https://tudominio.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
